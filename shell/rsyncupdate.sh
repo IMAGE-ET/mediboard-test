30ca76d81eaf1d179c49b2001e628d314432239a
flaviencrochard <flaviencrochard@a21d6be2-e512-0410-a060-91f5a2946631>
2013-11-05 11:26:35 +0000

    Fnc: Ajout de l'installation des librairies dans le script rsyncupdate.

--- shell/rsyncupdate.sh
+++ shell/rsyncupdate.sh
@@ -73,7 +73,6 @@ then
         eval rsync -avpgz $dry_run --stats $MB_PATH/ --delete $line --exclude-from=$BASH_PATH/rsyncupdate.exclude \
           --exclude includes/config_overload.php \
           --exclude /tmp \
-          --exclude /lib \
           --exclude /files \
           --exclude includes/config.php \
           --exclude modules/hprimxml/xsd \
