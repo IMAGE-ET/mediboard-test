dad6461f272e218e8c1a77d6676c8e55794c132e
Fabien Ménager <fabien.menager@gmail.com>
2013-12-03 08:16:55 +0000

    bug: erreur fatale lors du store d'une consult si elle n'a pas d'actes

--- modules/dPcabinet/classes/CConsultation.class.php
+++ modules/dPcabinet/classes/CConsultation.class.php
@@ -950,8 +950,8 @@ class CConsultation extends CFacturable {
     // Enregistrement de la facture
     if ($this->fieldModified("valide", "1")) {
       //Si le DH est modifié, ceui ci se répercute sur le premier acte coté
-      if ($this->fieldModified("secteur2") && (count($this->_tokens_ngap) || count($this->_tokens_ccam))) {
-        $acte = reset($this->loadRefsActes());
+      if ($this->fieldModified("secteur2") && (count($this->_tokens_ngap) || count($this->_tokens_ccam)) && count($this->loadRefsActes())) {
+        $acte = reset($this->_ref_actes);
         $acte->montant_depassement += ($this->secteur2-$this->_old->secteur2);
         if ($msg = $acte->store()) {
           return $msg;
