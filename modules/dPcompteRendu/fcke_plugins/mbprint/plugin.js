df59802fccf4973b5f03bf545276d6d014af065e
flaviencrochard <flaviencrochard@a21d6be2-e512-0410-a060-91f5a2946631>
2014-04-08 15:35:39 +0000

    Bug: Problème d'impression avec le bouton d'imprimante dans les documents.

--- modules/dPcompteRendu/fcke_plugins/mbprint/plugin.js
+++ modules/dPcompteRendu/fcke_plugins/mbprint/plugin.js
@@ -9,31 +9,35 @@
 
 CKEDITOR.plugins.add('mbprint',{
   requires: ['iframedialog'],
-  init:function(editor){ 
-   editor.addCommand('mbprint', {exec: mbprint_onclick});
-   editor.ui.addButton('mbprint', {label:'Imprimer (ancienne version)', command:'mbprint',
-   	 icon:'../../modules/dPcompteRendu/fcke_plugins/mbprint/images/mbprint.gif' });
+  init:function(editor){
+    editor.addCommand('mbprint', {exec: mbprint_onclick});
+    editor.ui.addButton('mbprint', {label:'Imprimer (ancienne version)', command:'mbprint',
+      icon:'../../modules/dPcompteRendu/fcke_plugins/mbprint/images/mbprint.gif' });
   }
 });
 
 function mbprint_onclick(editor) {
   editor.getCommand('mbprint').setState(CKEDITOR.TRISTATE_DISABLED);
-  var printDoc = function() {
-    if ( CKEDITOR.env.gecko )
-      editor.window.$.print();
-    else
-      editor.document.$.execCommand("Print");
-  };
+
   // Mise à jour de la date d'impression
-  window.parent.$V(window.parent.getForm("editFrm").date_print, "now");
-  if (window.parent.same_print == 1) {
-    var content = editor.getData();
-    var form = window.parent.document.forms["download-pdf-form"];
-    form.elements.content.value = encodeURIComponent(content);
-    form.onsubmit();
-  }
-  else {
-    window.parent.submitCompteRendu(printDoc);
-  }
+  $V(getForm("editFrm").date_print, "now");
+
+  var printDoc = function() {
+    if (window.parent.same_print == 1) {
+      var content = editor.getData();
+      var form = getForm("download-pdf-form");
+      form.elements.content.value = encodeURIComponent(content);
+      form.onsubmit();
+    }
+    else {
+      if (CKEDITOR.env.gecko) {
+        editor.window.$.print();
+      }
+      else {
+        editor.document.$.execCommand("Print");
+      }
+    }
+  };
+  submitCompteRendu(printDoc);
 }
 
