fb9c289bcc6966d89ee240fcd74ab36091c28477
lryo <lryo@a21d6be2-e512-0410-a060-91f5a2946631>
2014-04-30 08:09:19 +0000

    bug: Variable indéfinie

--- modules/ihe/classes/CITI30DelegatedHandler.class.php
+++ modules/ihe/classes/CITI30DelegatedHandler.class.php
@@ -2,17 +2,17 @@
 
 /**
  * ITI30 Delegated Handler
- *  
+ *
  * @category IHE
  * @package  Mediboard
  * @author   SARL OpenXtrem <dev@openxtrem.com>
- * @license  GNU General Public License, see http://www.gnu.org/licenses/gpl.html 
- * @version  SVN: $Id:$ 
+ * @license  GNU General Public License, see http://www.gnu.org/licenses/gpl.html
+ * @version  SVN: $Id:$
  * @link     http://www.mediboard.org
  */
 
 /**
- * Class CITI30DelegatedHandler 
+ * Class CITI30DelegatedHandler
  * ITI30 Delegated Handler
  */
 class CITI30DelegatedHandler extends CITIDelegatedHandler {
@@ -59,26 +59,28 @@ class CITI30DelegatedHandler extends CITIDelegatedHandler {
     /** @var CInteropReceiver $receiver */
     $receiver = $mbObject->_receiver;
     $receiver->getInternationalizationCode($this->transaction);
-    
+
     $eai_initiateur_group_id = $mbObject->_eai_initiateur_group_id;
-    
+
+    $code = null;
+
     // Création/MAJ d'un correspondant patient
     if ($mbObject instanceof CCorrespondantPatient) {
       if (!$mbObject->patient_id) {
-        return; 
+        return;
       }
-      
+
       $mbObject                           = $mbObject->loadRefPatient();
       $mbObject->_receiver                = $receiver;
       $mbObject->_eai_initiateur_group_id = $eai_initiateur_group_id;
-      
+
       $code = "A31";
     }
-    
+
     // MAJ de l'IPP du patient
     elseif ($mbObject instanceof CIdSante400) {
       $idex = $mbObject;
-      
+
       // Concerne pas les patients / Pas en mode modification
       if ($idex->object_class != "CPatient" || !$idex->_old->_id) {
         return;
@@ -88,32 +90,32 @@ class CITI30DelegatedHandler extends CITIDelegatedHandler {
       if ($idex->tag != CPatient::getTagIPP()) {
         return;
       }
-     
+
       // Vraiment une modif de l'idex ?
       if ($idex->id400 == $idex->_old->id400) {
         return;
       }
-      
+
       $code = "A47";
-      
+
       $patient = new CPatient();
       $patient->load($idex->object_id);
       $patient->_receiver = $receiver;
-      
+
       $patient->_patient_elimine = clone $patient;
-      
+
       // Affecte le nouvel IPP au patient
       $patient->_IPP = $idex->id400;
-      
+
       // Affecte l'ancien IPP au "patient éliminé"
       $patient->_patient_elimine->_IPP = $idex->_old->id400;
 
       if (!$this->isMessageSupported($this->transaction, $this->message, $code, $receiver)) {
         return;
       }
-      
+
       $this->sendITI($this->profil, $this->transaction, $this->message, $code, $patient);
-      
+
       return;
     }
 
@@ -129,7 +131,7 @@ class CITI30DelegatedHandler extends CITIDelegatedHandler {
             $code = "A24";
             break;
           }
-          
+
           // Annulation de la liaison avec le patient lié
           if ($mbObject->_old->patient_link_id && !$mbObject->patient_link_id) {
             $code = "A37";
@@ -148,8 +150,11 @@ class CITI30DelegatedHandler extends CITIDelegatedHandler {
           if ($receiver->_configs["send_patient_with_current_admit"]) {
             // On charge seulement le séjour courant pour le patient
             $sejours = $mbObject->getCurrSejour(null, $receiver->group_id);
-            if (!$sejours || ($sejour = reset($sejours) && !$sejour->_id)) {
-              $code = null;
+            if (!$sejours) {
+              break;
+            }
+
+            if ($sejour = reset($sejours) && !$sejour->_id) {
               break;
             }
 
@@ -160,23 +165,24 @@ class CITI30DelegatedHandler extends CITIDelegatedHandler {
           $code = ($receiver->_configs["send_update_patient_information"] == "A08") ? "A08" : "A31";
 
           break;
-        default:
-          $code = null;
-          break;
       }
     }
-    
+
+    if (!$code) {
+      return;
+    }
+
     $patient = $mbObject;
     if ($patient->_eai_initiateur_group_id || !$this->isMessageSupported($this->transaction, $this->message, $code, $receiver)) {
       return;
     }
-     
+
     if (!$patient->_IPP) {
       // Génération de l'IPP dans le cas de la création, ce dernier n'était pas créé
       if ($msg = $patient->generateIPP()) {
         CAppUI::setMsg($msg, UI_MSG_ERROR);
       }
-      
+
       $IPP = new CIdSante400();
       $IPP->loadLatestFor($patient, $receiver->_tag_patient);
       $patient->_IPP = $IPP->id400;
@@ -186,9 +192,9 @@ class CITI30DelegatedHandler extends CITIDelegatedHandler {
     if (!$receiver->_configs["send_all_patients"] && !$patient->_IPP) {
       return;
     }
-    
+
     $this->sendITI($this->profil, $this->transaction, $this->message, $code, $patient);
-    
+
     $patient->_IPP = null;
   }
 
@@ -206,6 +212,43 @@ class CITI30DelegatedHandler extends CITIDelegatedHandler {
   }
 
   /**
+   * Trigger when merge failed
+   *
+   * @param CMbObject $mbObject Object
+   *
+   * @return bool
+   */
+  function onMergeFailure(CMbObject $mbObject) {
+    if (!$this->isHandled($mbObject)) {
+      return false;
+    }
+
+    // On va réatribuer les idexs en cas de problème dans la fusion
+    foreach ($mbObject->_fusion as $group_id => $infos_fus) {
+      if (!$infos_fus || !array_key_exists("idexs_changed", $infos_fus)) {
+        return false;
+      }
+
+      foreach ($infos_fus["idexs_changed"] as $idex_id => $tag_name) {
+        $idex = new CIdSante400();
+        $idex->load($idex_id);
+
+        if (!$idex->_id) {
+          continue;
+        }
+
+        // Réattribution sur l'objet non supprimé
+        $patient_eliminee = $infos_fus["patientElimine"];
+        $idex->object_id = $patient_eliminee->_id;
+
+        $idex->tag = $tag_name;
+        $idex->last_update = CMbDT::dateTime();
+        $idex->store();
+      }
+    }
+  }
+
+  /**
    * Trigger after event merge
    *
    * @param CMbObject $mbObject Object
@@ -216,30 +259,30 @@ class CITI30DelegatedHandler extends CITIDelegatedHandler {
     if (!$this->isHandled($mbObject)) {
       return false;
     }
-    
-    if ($mbObject instanceof CPatient) {  
+
+    if ($mbObject instanceof CPatient) {
       $patient = $mbObject;
       $patient->check();
       $patient->updateFormFields();
-      
-      $receiver = $mbObject->_receiver; 
+
+      $receiver = $mbObject->_receiver;
       $receiver->getInternationalizationCode($this->transaction);
 
       foreach ($patient->_fusion as $group_id => $infos_fus) {
         if ($receiver->group_id != $group_id) {
           continue;
-        }      
+        }
 
         $patient1_ipp     = $patient->_IPP = $infos_fus["patient1_ipp"];
-        
+
         $patient_eliminee = $infos_fus["patientElimine"];
         $patient2_ipp     = $patient_eliminee->_IPP = $infos_fus["patient2_ipp"];
-  
+
         // Cas 0 IPP : Aucune notification envoyée
         if (!$patient1_ipp && !$patient2_ipp) {
           continue;
         }
-  
+
         // Cas 1 IPP : Pas de message de fusion mais d'une modification du patient
         if ($patient1_ipp xor $patient2_ipp) {
           if ($patient2_ipp) {
@@ -250,24 +293,24 @@ class CITI30DelegatedHandler extends CITIDelegatedHandler {
           if (!$this->isMessageSupported($this->transaction, $this->message, $code, $receiver)) {
             return;
           }
-            
+
           $this->sendITI($this->profil, $this->transaction, $this->message, $code, $patient);
           continue;
         }
-        
+
         // Cas 2 IPPs : Message de fusion
         if ($patient1_ipp && $patient2_ipp) {
           $patient->_patient_elimine = $patient_eliminee;
-          
+
           if (!$this->isMessageSupported($this->transaction, $this->message, "A40", $receiver)) {
             return;
           }
-          
+
           $this->sendITI($this->profil, $this->transaction, $this->message, "A40", $patient);
           continue;
         }
-      } 
-    } 
+      }
+    }
   }
 
   /**
@@ -281,7 +324,7 @@ class CITI30DelegatedHandler extends CITIDelegatedHandler {
     if (!$this->isHandled($mbObject)) {
       return false;
     }
-    
+
     return true;
   }
 
@@ -301,24 +344,24 @@ class CITI30DelegatedHandler extends CITIDelegatedHandler {
     if (!$mbObject instanceof CCorrespondantPatient) {
       return;
     }
-    
+
     $receiver = $mbObject->_receiver;
     $receiver->getInternationalizationCode($this->transaction);
-    
+
     $patient = $mbObject->loadRefPatient();
     $patient->_receiver = $receiver;
-    
+
     $code = ($receiver->_configs["send_update_patient_information"] == "A08") ? "A08" : "A31";
-        
+
     if ($patient->_eai_initiateur_group_id || !$this->isMessageSupported($this->transaction, $this->message, $code, $receiver)) {
       return;
     }
-    
+
     // Envoi pas les patients qui n'ont pas d'IPP
     if (!$receiver->_configs["send_all_patients"] && !$patient->_IPP) {
       return;
     }
-    
+
     $this->sendITI($this->profil, $this->transaction, $this->message, $code, $patient);
   }
 }
