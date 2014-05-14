51ad6cacd36cf7336963cbcc3a1e857b296f80ad
lryo <lryo@a21d6be2-e512-0410-a060-91f5a2946631>
2014-05-14 08:39:48 +0000

    bug: non chargement des protocoles de DHE de fonction par défaut

--- modules/dPplanningOp/ajax_protocoles_autocomplete.php
+++ modules/dPplanningOp/ajax_protocoles_autocomplete.php
@@ -42,7 +42,9 @@ else {
   $curr_user = CMediusers::get();
   $use_edit = CAppUI::pref("useEditAutocompleteUsers");
   $prats = $curr_user->loadPraticiens($use_edit ? PERM_EDIT : PERM_READ);
-  $where["protocole.chir_id"] = CSQLDataSource::prepareIn(CMbArray::pluck($prats, "user_id"));
+  $fncs  = $curr_user->loadFonctions($use_edit ? PERM_EDIT : PERM_READ);
+  $where[] = "(protocole.chir_id ".CSQLDataSource::prepareIn(CMbArray::pluck($prats, "user_id")).
+    " OR protocole.function_id ". CSQLDataSource::prepareIn(array_keys($fncs)).")";
 }
 
 if ($for_sejour !== null) {
