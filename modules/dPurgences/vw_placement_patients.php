238b41ff5047b72d6abc07755b3257003a4e33c8
aurelie17 <aurelie17@a21d6be2-e512-0410-a060-91f5a2946631>
2014-05-27 09:33:23 +0000

    Bug: Le box_id est le lit et non la chambre du rpu

--- modules/dPurgences/vw_placement_patients.php
+++ modules/dPurgences/vw_placement_patients.php
@@ -132,12 +132,11 @@ foreach ($sejours as $sejour) {
 
     $sejour->countDocItems();
   }
-  $chambre_id = null;
-  if (CAppUI::conf("dPurgences create_affectation")) {
-    $chambre_id = $sejour->_ref_curr_affectation->loadRefLit()->chambre_id;
-  }
-  else {
-    $chambre_id = $sejour->_ref_rpu->box_id;
+  $chambre_id = $sejour->_ref_curr_affectation->loadRefLit()->chambre_id;
+  if (!$chambre_id && !CAppUI::conf("dPurgences create_affectation")) {
+    $lit = new CLit();
+    $lit->load($sejour->_ref_rpu->box_id);
+    $chambre_id = $lit->chambre_id;
   }
   $sejours_chambre[$chambre_id][] = $sejour;
 }
