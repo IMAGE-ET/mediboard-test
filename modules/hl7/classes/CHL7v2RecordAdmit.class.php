3dcf124a1dd98a9879c958abb1a80ac21fc73ae0
nicolasld <nicolasld@a21d6be2-e512-0410-a060-91f5a2946631>
2014-08-18 14:13:25 +0000

    Bug : Impossible de storer les prestations lors de la reception de messages HL7

--- modules/hl7/classes/CHL7v2RecordAdmit.class.php
+++ modules/hl7/classes/CHL7v2RecordAdmit.class.php
@@ -2138,7 +2138,8 @@ class CHL7v2RecordAdmit extends CHL7v2MessageXML {
 
     $item_liaison = new CItemLiaison();
     $ljoin = array(
-      "prestation_journaliere" => "prestation_journaliere.prestation_journaliere_id = item_liaison.item_souhait_id"
+      "item_prestation"        => "item_prestation.item_prestation_id = item_liaison.item_souhait_id",
+      "prestation_journaliere" => "item_prestation.object_id = prestation_journaliere_id"
     );
 
     $where["item_liaison.sejour_id"]                           = " = '$newVenue->_id'";
@@ -2146,6 +2147,11 @@ class CHL7v2RecordAdmit extends CHL7v2MessageXML {
     $where["prestation_journaliere.prestation_journaliere_id"] = " = '$presta_journa->_id'";
 
     $item_liaison->loadObject($where, null, null, $ljoin);
+
+    if (!$item_liaison->_id) {
+      $item_liaison->sejour_id = $newVenue->_id;
+      $item_liaison->date      = CMbDT::date($newVenue->entree);
+    }
 
     $item_liaison->item_souhait_id = $item_presta->_id;
 
