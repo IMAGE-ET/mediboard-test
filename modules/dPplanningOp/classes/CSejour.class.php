93375c3d94944ee223b6439490b12409b531dcc9
charlyecho <charlyecho@a21d6be2-e512-0410-a060-91f5a2946631>
2014-09-01 08:04:49 +0000

    Ref: ordre des consultations et des operations d'un séjour dans l'ordre de date décroissant

--- modules/dPplanningOp/classes/CSejour.class.php
+++ modules/dPplanningOp/classes/CSejour.class.php
@@ -2250,10 +2250,12 @@ class CSejour extends CFacturable implements IPatientRelated {
   /**
    * Charge les consultations, en particulier l'ATU dans le cas UPATOU
    *
+   * @param string $order order of the list
+   *
    * @return CConsultation[]
    */
-  function loadRefsConsultations() {
-    $this->_ref_consultations = $this->loadBackRefs("consultations");
+  function loadRefsConsultations($order = "date DESC, heure DESC") {
+    $this->_ref_consultations = $this->loadBackRefs("consultations", $order, null, null, array("plageconsult" => "plageconsult.plageconsult_id = consultation.plageconsult_id"));
 
     $this->_ref_consult_atu = new CConsultation();
 
