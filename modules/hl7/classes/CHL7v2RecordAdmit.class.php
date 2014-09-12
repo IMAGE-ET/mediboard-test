721bcc4bbc55cc6321b437809bcc052160de45ac
nicolasld <nicolasld@a21d6be2-e512-0410-a060-91f5a2946631>
2014-09-12 13:53:49 +0000

    Bug : Le champ date de fin peut être null

--- modules/hl7/classes/CHL7v2RecordAdmit.class.php
+++ modules/hl7/classes/CHL7v2RecordAdmit.class.php
@@ -2780,7 +2780,9 @@ class CHL7v2RecordAdmit extends CHL7v2MessageXML {
       $where["prenom"]     = " = '$prenom'";
     }
     $where["relation"]   = " = 'assurance'";
-    $where[]             = "'".CMbDT::date()."' BETWEEN date_debut AND date_fin";
+    $now  = CMbDT::date();
+    $next = CMbDT::date("+ 1 DAY");
+    $where["date_debut"] = " <= '$now' AND IFNULL(date_fin, '$next') >= '$now'";
 
     $corres_patient->loadObject($where);
 
