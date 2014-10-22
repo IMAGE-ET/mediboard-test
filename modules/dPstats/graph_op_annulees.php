120266d35bba1dbd3cb9680b7498733cf148b941
mytto <tomdesp@users.noreply.github.com>
2014-10-22 15:12:56 +0000

    Bug: Traces oubliées dans les statistiques d'interventions annulées

--- modules/dPstats/graph_op_annulees.php
+++ modules/dPstats/graph_op_annulees.php
@@ -144,10 +144,6 @@ function graphOpAnnulees(
     $subtitle .= " - ".CAppUI::tr("CSejour.type.$type_sejour");
   }
 
-  mbTrace($ticks);
-  mbTrace($series);
-  mbTrace($total);
-
   $options = array(
     'title' => utf8_encode($title),
     'subtitle' => utf8_encode($subtitle),
