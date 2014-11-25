10947aad727a5d3cb626fe42f83d6314569cf5dc
lryo <lryo@a21d6be2-e512-0410-a060-91f5a2946631>
2014-11-25 11:41:20 +0000

    ref: On récupére la date d'entrée et sortie réelle ssi on autorise la réception et que l'état de la vue est correcte

--- modules/hprimxml/classes/CHPrimXMLEvenementsPatients.class.php
+++ modules/hprimxml/classes/CHPrimXMLEvenementsPatients.class.php
@@ -423,7 +423,9 @@ class CHPrimXMLEvenementsPatients extends CHPrimXMLEvenements {
 
     $dateHeure = "$date $heure";
 
-    if ($mbVenue->entree_reelle && CAppUI::conf("hprimxml notifier_entree_reelle")) {
+    if (CAppUI::conf("hprimxml notifier_entree_reelle") &&
+          (self::getEtatVenue($node) == "encours" || self::getEtatVenue($node) == "clôturée")
+    ) {
       $mbVenue->entree_reelle = $dateHeure;
     }
     else {
@@ -552,7 +554,7 @@ class CHPrimXMLEvenementsPatients extends CHPrimXMLEvenements {
     }
 
     // Cas dans lequel on ne récupère pas de sortie tant que l'on a pas la sortie réelle
-    if ($mbVenue->sortie_reelle && CAppUI::conf("hprimxml notifier_sortie_reelle")) {
+    if (CAppUI::conf("hprimxml notifier_sortie_reelle") && self::getEtatVenue($node) == "clôturée") {
       $mbVenue->sortie_reelle = $dateHeure;
     }
     else {
