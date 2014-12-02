b8cec31562dc4ba2db6f1edd4feec43e6470e1b7
lryo <lryo@a21d6be2-e512-0410-a060-91f5a2946631>
2014-12-02 10:41:24 +0000

    ref: on utilise la référence aux dossiers d'anesthésie au lieu des consultations d'anesthésie

--- modules/dPcabinet/ajax_old_consult_intubation.php
+++ modules/dPcabinet/ajax_old_consult_intubation.php
@@ -1,34 +1,38 @@
-<?php 
-
+<?php
 /**
  * $Id$
- *  
- * @category ${Module}
- * @package  Mediboard
- * @author   SARL OpenXtrem <dev@openxtrem.com>
- * @license  GNU General Public License, see http://www.gnu.org/licenses/gpl.html 
- * @link     http://www.mediboard.org */
+ *
+ * @category dPcabinet
+ * @package Mediboard
+ * @author SARL OpenXtrem <dev@openxtrem.com>
+ * @license GNU General Public License, see http://www.gnu.org/licenses/gpl.html
+ * @link http://www.mediboard.org
+ *
+ */
 
 CCanDo::checkRead();
 
-$patient_id = CValue::get("patient_id");
+$patient_id        = CValue::get("patient_id");
 $consult_anesth_id = CValue::get("consult_anesth_id");
+
 $patient = new CPatient();
 $patient->load($patient_id);
 
-$consultations_anesth = array();
+$dossiers_anesth = array();
 
 foreach ($patient->loadRefsConsultations() as $_consult) {
-  $_consult->loadRefPraticien()->loadRefFunction();
-  $_consult->loadRefPlageConsult(true);
+  foreach ($_consult->loadRefsDossiersAnesth() as $_dossier) {
+    if ($_dossier->_id == $consult_anesth_id) {
+      continue;
+    }
 
-  $consult_anesth = $_consult->loadRefConsultAnesth();
-  if ($consult_anesth->_id && $consult_anesth->_id != $consult_anesth_id) {
-    $consultations_anesth[] = $consult_anesth;
+    $_dossier->_ref_consultation = $_consult;
+    $_consult->loadRefPraticien()->loadRefFunction();
+    $_consult->loadRefPlageConsult(true);
+    $dossiers_anesth[] = $_dossier;
   }
 }
-
 $smarty = new CSmartyDP;
-$smarty->assign("consultations_anesth", $consultations_anesth);
-$smarty->assign("patient"             , $patient);
-$smarty->display("inc_consult_anesth/vw_old_consult_anesth.tpl");
+$smarty->assign("dossiers_anesth", $dossiers_anesth);
+$smarty->assign("patient"        , $patient);
+$smarty->display("inc_consult_anesth/vw_old_consult_anesth.tpl"); 
