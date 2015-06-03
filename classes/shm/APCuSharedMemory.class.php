a074c91e0b458555a716e84b82c54bb2a43e6c33
Fabien Ménager <fabien.menager@gmail.com>
2015-06-03 10:09:58 +0000

    bug: meilleure gestion des clés APCu

--- classes/shm/APCuSharedMemory.class.php
+++ classes/shm/APCuSharedMemory.class.php
@@ -1,8 +1,8 @@
-<?php 
+<?php
 
 /**
  * $Id$
- *  
+ *
  * @category Classes
  * @package  Mediboard
  * @author   SARL OpenXtrem <dev@openxtrem.com>
@@ -18,6 +18,25 @@ require_once __DIR__."/APCSharedMemory.class.php";
  */
 class APCuSharedMemory extends APCSharedMemory {
   protected $_cache_key = "key";
+
+  /**
+   * @see parent::modDate()
+   */
+  function modDate($key) {
+    $info       = apc_cache_info("user");
+    $cache_list = $info["cache_list"];
+    $cache_key  = $this->_cache_key;
+
+    foreach ($cache_list as $_cache) {
+      $_key = $_cache[$cache_key];
+
+      if ($_key === $key) {
+        return strftime(CMbDT::ISO_DATETIME, isset($_cache["mtime"]) ? $_cache["mtime"] : $_cache["modification_time"]);
+      }
+    }
+
+    return null;
+  }
 
   /**
    * @see parent::info()
@@ -39,8 +58,8 @@ class APCuSharedMemory extends APCSharedMemory {
 
     foreach ($user_cache["cache_list"] as $_cache_info) {
       if ($_cache_info["key"] == $key) {
-        $cache_info["creation_date"]     = strftime(CMbDT::ISO_DATETIME, $_cache_info["ctime"]);
-        $cache_info["modification_date"] = strftime(CMbDT::ISO_DATETIME, $_cache_info["mtime"]);
+        $cache_info["creation_date"]     = strftime(CMbDT::ISO_DATETIME, isset($_cache_info["ctime"]) ? $_cache_info["ctime"] : $_cache_info["creation_time"]);
+        $cache_info["modification_date"] = strftime(CMbDT::ISO_DATETIME, isset($_cache_info["mtime"]) ? $_cache_info["mtime"] : $_cache_info["modification_time"]);
         $cache_info["num_hits"]          = $_cache_info["nhits"];
         $cache_info["mem_size"]          = $_cache_info["mem_size"];
 
