703f6681e1ed2fa0b6cd9e23b217f2403e9ab21f
lryo <lryo@a21d6be2-e512-0410-a060-91f5a2946631>
2015-06-05 07:04:49 +0000

    ref: Modification du where sur la recherche des échanges à ré-envoyer ref: Modification de la borne pour la transmission des messages non envoyés

--- modules/eai/ajax_inject_master_idex_missing.php
+++ modules/eai/ajax_inject_master_idex_missing.php
@@ -8,7 +8,7 @@ $date_min       = CValue::get('date_min');
 $date_max       = CValue::get('date_max');
 
 if (!$date_min) {
-  $date_min = CMbDT::dateTime("-3 day");
+  $date_min = CMbDT::dateTime("-1 day");
 }
 
 if (!$date_max) {
@@ -44,5 +44,5 @@ foreach ($exchanges as $_exchange) {
     continue;
   }
 
-  CAppUI::stepAjax("CExchangeDataFormat-confirm-Data injected");
+  CAppUI::stepAjax("$_exchange->_guid : CExchangeDataFormat-confirm-Data injected");
 }
