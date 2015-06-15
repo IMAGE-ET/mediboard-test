8fb3eb7194fbd1a100db063ad0ef49c06b11fb46
Fabien Ménager <fabien.menager@gmail.com>
2015-06-15 15:29:41 +0000

    bug: il manquait un champ dans l'export de patient fnc: possibilité de choisir de forcer la génération des PDF des CR lors de l'export de patient

--- modules/dPpatients/controllers/do_export_patients.php
+++ modules/dPpatients/controllers/do_export_patients.php
@@ -19,6 +19,7 @@ $step         = (int)CValue::post("step");
 $start        = (int)CValue::post("start");
 $directory    = CValue::post("directory");
 $ignore_files = CValue::post("ignore_files");
+$generate_pdfpreviews = CValue::post("generate_pdfpreviews");
 
 if (!$all_prats && !$praticien_id) {
   CAppUI::stepAjax("Veuillez choisir au moins un praticien, ou cocher 'Tous les praticiens'", UI_MSG_WARNING);
@@ -38,6 +39,7 @@ CValue::setSession("step", $step);
 CValue::setSession("start", $start);
 CValue::setSession("directory", $directory);
 CValue::setSession("ignore_files", $ignore_files);
+CValue::setSession("generate_pdfpreviews", $generate_pdfpreviews);
 
 $step = min($step, 1000);
 
@@ -177,6 +179,10 @@ $fwdrefs_tree = array(
   "CGrossesse" => array(
     "group_id",
     "parturiente_id",
+  ),
+  "CCorrespondant" => array(
+    "patient_id",
+    "medecin_id",
   ),
   "CMediusers" => array(
     "user_id",
@@ -321,11 +327,13 @@ foreach ($patients as $_patient) {
 
     $export = new CMbObjectExport($_patient, $backrefs_tree);
 
-    $callback = function (CStoredObject $object, $node, $depth) use ($export, $dir, $ignore_files) {
+    $callback = function (CStoredObject $object, $node, $depth) use ($export, $dir, $ignore_files, $generate_pdfpreviews) {
       switch ($object->_class) {
         case "CCompteRendu":
           /** @var CCompteRendu $object */
-          $object->makePDFpreview(true);
+          if ($generate_pdfpreviews) {
+            $object->makePDFpreview(true);
+          }
           break;
 
         case "CFile":
